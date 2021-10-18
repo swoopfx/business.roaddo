d13febb7ba1e8de13323409df0d905c351318997
Ezekiel Ajayi <ezekiel_a@yahoo.com>
2021-10-18 14:36:49 +0100

    Init

---
+++ config/modules.config.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Di',
+    'Laminas\Db',
+    'Laminas\Cache',
+    'Laminas\Mvc\Middleware',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Mvc\I18n',
+    'Laminas\I18n',
+    'Laminas\Log',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\Diactoros',
+    'Application',
+];
